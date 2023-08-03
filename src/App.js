527bc106e61fe9979d207cc3babce651702b4d42
Rayen <rayen.bakali@gmail.com>
2023-08-03 23:32:48 +0100

    readme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import { useState, useEffect } from "react";
+import PokemonList from "./PokemonList";
+import Pagination from "./Pagination";
+import axios from "axios";
+
 function App() {
-  return <h1>Hello world</h1>;
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPage, setCurrentPage] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPage, setNextPage] = useState();
+  const [prevPage, setPrevPage] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let cancel;
+    setLoading(true);
+    axios
+      .get(currentPage, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setLoading(false);
+        setNextPage(res.data.next);
+        setPrevPage(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+      });
+    return () => {
+      cancel();
+    };
+  }, [currentPage]);
+
+  function goToNextPage() {
+    setCurrentPage(nextPage);
+  }
+
+  function goToPrevPage() {
+    setCurrentPage(prevPage);
+  }
+  if (loading) return "Loading...";
+  return (
+    <div>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        goToNextPage={nextPage ? goToNextPage : null}
+        goToPrevPage={prevPage ? goToPrevPage : null}
+      />
+    </div>
+  );
 }
 
 export default App;
